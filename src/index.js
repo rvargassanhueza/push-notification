2488f062ae38357cd860a64dae275e4a6428a6e7
Rodrigo Vargas <rodrigovargassanhueza@gmail.com>
2021-11-05 22:57:00 -0300

    ajuste puerto para heroku

--- src/index.js
+++ src/index.js
@@ -19,5 +19,5 @@ app.use(require('./routes/index'))
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
 console.log("Server listening...");

b3e25d3804783d1d80d8148734934ac040bf3c5c
Rodrigo Vargas <rodrigovargassanhueza@gmail.com>
2021-11-08 14:30:21 -0300

    ajustes directorios archivos

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,21 +8,25 @@ let pushSubscription;
 router.post('/subscription', async (req,res)=>{
     console.log(req.body)
     pushSubscription = req.body;
+    console.log("pushSubscription 1: ",pushSubscription)
+
     res.status(200).json();
 })
 
 router.post('/new-message', async (req,res)=>{
     const {title,message,destino,imagen} = req.body
-
+console.log("request: ",req.body )
         const payload = JSON.stringify({
             title:title,
             message:message,
             destino:destino,
             imagen:imagen
-        })
+        }) 
 
         try {
-            await webpush.sendNotification(pushSubscription,payload)
+            console.log("pushSubscription: ",pushSubscription)
+            const response = await webpush.sendNotification(pushSubscription,payload)
+            console.log("response: ",response)
         } catch (error) {
             console.log(error)
         }

557ae55442c82c6d5a3bca51e4e76c43349d4e0e
Rodrigo Vargas <rodrigovargassanhueza@gmail.com>
2021-11-09 15:47:48 -0300

    se agrea response 201 en new-message

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,16 +6,13 @@ const webpush = require('../webpush');
 let pushSubscription;
 
 router.post('/subscription', async (req,res)=>{
-    console.log(req.body)
     pushSubscription = req.body;
-    console.log("pushSubscription 1: ",pushSubscription)
 
     res.status(200).json();
 })
 
 router.post('/new-message', async (req,res)=>{
     const {title,message,destino,imagen} = req.body
-console.log("request: ",req.body )
         const payload = JSON.stringify({
             title:title,
             message:message,
@@ -24,9 +21,10 @@ console.log("request: ",req.body )
         }) 
 
         try {
-            console.log("pushSubscription: ",pushSubscription)
             const response = await webpush.sendNotification(pushSubscription,payload)
-            console.log("response: ",response)
+            console.log("response: ",response);
+            res.status(201).json();
+
         } catch (error) {
             console.log(error)
         }
